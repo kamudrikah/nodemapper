ed196935867c07b6bc9a4b23c43720d7c3327df8
Mudrikah <kamudrikah@gmail.com>
2020-03-13 17:22:15 +0800

    Application

---
+++ src/index.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+const PDF2Pic = require('pdf2pic');
+const path = require('path');
+
+const express = require('express');
+const app = express();
+
+app.get('/', function (req, res) {
+    const dest = path.resolve('./storage/form.pdf');
+    const tempPath = path.resolve('./storage/');
+
+    const pdf2pic = new PDF2Pic({
+        savename: 'temp',
+        savedir: tempPath,
+        format: "png",
+        density: 200,
+        size: "1240x1754",
+    });
+    pdf2pic.convert(dest).then((resolve) => {
+        console.log("image converter successfully!");
+        return resolve;
+    });
+    // return pdf2pic.convertBulk(tempPath, -1);
+    res.send("kjhad");
+});
+app.listen(3000);
+
+console.log('Node app is running..');
